2e61403aa91ae43cbb6abc04aaa264e099c736c8
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-27 22:56:41 -0600

    Ejercicio 1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Ejercicio 1
+
+Console.Write("Ingrese una cadena de caracteres: ");
+string cadena = Console.ReadLine();
+
+if (cadena.Length < 25)
+{
+    cadena = new string('$', 25 - cadena.Length) + cadena;
+}
+
+Console.WriteLine($"Cadena resultante: {cadena}");
