4b8240f081581b1890a4efc10223255201598263
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-27 23:10:02 -0600

    Ejercicio 3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Ejercicio 3
+
+string Frase = "MI MAMA ME MIMA, AMO A MI MAMA";
+
+Frase = Frase.ToLower();
+
+string[] Palabras = Frase.Split(' ');
+
+string Resultado = "";
+
+foreach (string i in Palabras)
+{
+    if (i.Length < 3)
+    {
+        Resultado += i + "% ";
+    }
+    else
+    {
+        Resultado += i + " ";
+    }
+}
+Resultado = Resultado.Trim();
+
+Console.WriteLine(Resultado);
