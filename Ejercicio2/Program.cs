d2a1231e27ac40c56c14203a5a9060e1f913eaf2
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-27 23:03:51 -0600

    Ejercicio2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Ejercicio 2
+
+Console.WriteLine("Ingrese la primera cadena:");
+string Cadena1 = Console.ReadLine();
+
+Console.WriteLine("Ingrese la segunda cadena:");
+string Cadena2 = Console.ReadLine();
+
+int ResCompareTo = Cadena1.CompareTo(Cadena2);
+
+if (ResCompareTo == 0)
+{
+    Console.WriteLine("Las cadenas son iguales.");
+}
+else if (ResCompareTo < 0)
+{
+    Console.WriteLine("La primera cadena es menor que la segunda.");
+}
+else
+{
+    Console.WriteLine("La primera cadena es mayor que la segunda.");
+}
+
+bool Iguales = Cadena1.Equals(Cadena2);
+
+if (Iguales)
+{
+    Console.WriteLine("Las cadenas son iguales.");
+}
+else
+{
+    Console.WriteLine("Las cadenas no son iguales.");
+}
